8fb41bc365d4a0666b843d9b873cb6026c7f7ca7
EOppedal <erlend.oppedal@gmail.com>
2024-04-04 11:11:39 +0200

    TestCameraControls

---
+++ Assets/Scripts/TestCameraControls.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class TestCameraControls : MonoBehaviour {
+    [SerializeField] private float cameraSpeed;
+    private void Update() {
+        if (Keyboard.current.leftArrowKey.isPressed) {
+            transform.position += new Vector3(-cameraSpeed, 0, 0) * Time.deltaTime;
+        }
+        
+        if (Keyboard.current.rightArrowKey.isPressed) {
+            transform.position += new Vector3(cameraSpeed, 0, 0) * Time.deltaTime;
+        }
+    }
+}
